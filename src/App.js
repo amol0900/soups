ef1a933488b31e5896ec82d3c48d67869ea609f3
Amanda <a.olsson59@gmail.com>
2020-09-24 10:52:14 +0200

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+import './font/AdobeClean/stylesheet.css'
 
+const GlobalStyle = createGlobalStyle`
+body {
+    font-family: 'Adobe Clean';
+    font-size:0.8em;
+    font-weight:normal;
+    color:#242424;
+  }
+
+  h1 {
+    font-family: 'Adobe Clean';
+    font-size:1.7em;
+    font-weight:normal;
+    color:#242424;
+  }
+`
 
 function App() {
   return (
-<div>Hello soups</div>
+<>
+  <GlobalStyle/>
+  <div>
+    <h1>Roasted tomato & quinoa</h1>
+    <p>Tomatsoppa med quinoa, vitlök och lök toppas<br /> med färsk basilika och krispiga krutonger.</p>
+    </div>
+</>
   );
 }
 
